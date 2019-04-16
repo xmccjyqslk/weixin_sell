64e4d6cc263de8acd2686dd7591ff031bad08a61
xmcc <1146507158@qq.com>
2019-04-16 16:02:02 +0800

    第一次：出现了错误，重新建立的项目

---
+++ src/main/java/com/xmcc/controller/testController.java
@@ -0,0 +1,17 @@
+package com.xmcc.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class testController {
+
+    @RequestMapping("/hello")
+    public String ff(){
+        log.info("info -> {}","欢迎来到邮箱");
+        return "hello. booyt";
+    }
+}
